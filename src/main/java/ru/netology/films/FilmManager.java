5a29433ba78a7fcb13cbdb606457b2da4aa8d12b
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-08-12 16:26:17 +0300

    Corrected

--- src/main/java/ru/netology/films/FilmManager.java
+++ src/main/java/ru/netology/films/FilmManager.java
@@ -1,50 +1,41 @@
 package ru.netology.films;
 
 public class FilmManager {
-    private String name; //имя
-    private String genre; //жанр
-    private FilmManager[] items = new FilmManager[0];
-    private int limit = 5; //лимит по дефолту
-    private int resultLength;
-
-    public FilmManager(String name, String genre) {//принимает название и жанр
-        this.name = name;
-        this.genre = genre;
-    }
+    private String[] films = new String[0];
+    private int limit; // лимит по дефолту
 
     public FilmManager() {
-        resultLength = limit;
+        this.limit = 5;
     }
 
     public FilmManager(int limit) {
-        if (limit < 7) {
-            resultLength = limit;
+        this.limit = limit;
+    }
+
+    public void add(String name) { //добавить фильм
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = name;
+        films = tmp;
+    }
+
+    public String[] findLast() { //вывод последних
+        int resultLength;
+        if (films.length < limit) {
+            resultLength = films.length;
         } else {
-            resultLength = 7;
+            resultLength = limit;
         }
-    }
-
-    public void add(FilmManager newItem) { //добавить новый фильм
-        FilmManager[] tmp = new FilmManager[items.length + 1];
-        for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[i];
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
         }
-        tmp[tmp.length - 1] = newItem;
-        items = tmp;
+        return tmp;
     }
 
-    public FilmManager[] findLast() { //вывод лимит фильмов
-        FilmManager[] all = items;
-        FilmManager[] result = new FilmManager[resultLength];
-        for (int i = 0; i < resultLength; i++) {
-            result[i] = all[all.length - 1 - i];
-        }
-        return result;
+    public String[] findAll() { //вывод всех
+        return films;
     }
-
-    public FilmManager[] findAll() { //вывод в порядке добавления
-        return items;
-    }
-
 }
-
--- src/test/java/ru/netology/films/FilmManagerTest.java
+++ src/test/java/ru/netology/films/FilmManagerTest.java
@@ -5,62 +5,47 @@ import org.junit.jupiter.api.Test;
 
 public class FilmManagerTest {
 
-    FilmManager item1 = new FilmManager("Бладшот", "боевик");
-    FilmManager item2 = new FilmManager("Вперёд", "мультфильм");
-    FilmManager item3 = new FilmManager("Отель ''Белград''", "комедия");
-    FilmManager item4 = new FilmManager("Джентльмены", "боевик");
-    FilmManager item5 = new FilmManager("Человек-невидимка", "ужасы");
-    FilmManager item6 = new FilmManager("Тролли.мировой тур", "мультфильм");
-    FilmManager item7 = new FilmManager("Номер один", "комедия");
-
     @Test
-    public void shouldAddFilms() { //тест на добавление фильмов в ленту
-        FilmManager items = new FilmManager();
-        items.add(item1);
-        items.add(item2);
-        items.add(item3);
-        items.add(item4);
-        items.add(item5);
-        items.add(item6);
-        items.add(item7);
+    public void shouldAddFilms() { //добавляет фильмы
+        FilmManager films = new FilmManager();
+        films.add("Бладшот");
+        films.add("Вперёд");
+        films.add("Джентльмены");
 
-        FilmManager[] expected = {item1, item2, item3, item4, item5, item6, item7};
-        FilmManager[] actual = items.findAll();
-
-        Assertions.assertArrayEquals(expected, actual);
-
-    }
-
-    @Test
-    public void shouldShowNewestBelowSeven() { //выводит новые при выставлении меньше 7
-        FilmManager items = new FilmManager(4);
-        items.add(item1);
-        items.add(item2);
-        items.add(item3);
-        items.add(item4);
-        items.add(item5);
-        items.add(item6);
-        items.add(item7);
-
-        FilmManager[] expected = {item7, item6, item5, item4};
-        FilmManager[] actual = items.findLast();
+        String[] expected = {"Бладшот", "Вперёд", "Джентльмены"};
+        String[] actual = films.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldShowNewestAboveSeven() { //выводит новые при выставлении больше 7
-        FilmManager items = new FilmManager(9);
-        items.add(item1);
-        items.add(item2);
-        items.add(item3);
-        items.add(item4);
-        items.add(item5);
-        items.add(item6);
-        items.add(item7);
+    public void shouldShowWhereLimitIsLessThanQuantity() { //выводит последние фильмы где лимит меньше количества фильмов
+        FilmManager films = new FilmManager(3);
+        films.add("Бладшот");
+        films.add("Вперёд");
+        films.add("Отель Белград");
+        films.add("Джентльмены");
+        films.add("Человек-невидимка");
+        films.add("Тролли.Мировой тур");
+        films.add("Номер один");
+        films.add("Вызов");
+        films.add("Круэлла");
 
-        FilmManager[] expected = {item7, item6, item5, item4, item3, item2, item1};
-        FilmManager[] actual = items.findLast();
+        String[] expected = {"Круэлла", "Вызов", "Номер один"};
+        String[] actual = films.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowWhereLimitIsMoreThanQuantity() { //выводит последние фильмы, где лимит больше количества ильмов
+        FilmManager films = new FilmManager();
+        films.add("Бладшот");
+        films.add("Вперёд");
+        films.add("Отель Белград");
+
+        String[] expected = {"Отель Белград", "Вперёд", "Бладшот"};
+        String[] actual = films.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
